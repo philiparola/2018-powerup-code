3b634809a1da2207f914e7f1ac6b7d8eed9788d1
RyoTakei <takeiryou926@gmail.com>
2018-03-06 12:00:06 -0800

    prob not working trap prof

--- robot/src/main/kotlin/com/team2898/engine/motion/TrapezoidProfile.kt
+++ robot/src/main/kotlin/com/team2898/engine/motion/TrapezoidProfile.kt
@@ -1,9 +1,8 @@
 package com.team2898.engine.motion
 
-import edu.wpi.first.wpilibj.DigitalOutput
 import edu.wpi.first.wpilibj.Timer
-import kotlin.math.max
-import kotlin.math.sign
+import kotlin.math.abs
+
 
 class TrapezoidProfile(
         var maxAcc: Double, // in FSTU
@@ -11,43 +10,107 @@ class TrapezoidProfile(
         var currentVel: () -> Double,
         var currentPos: () -> Double
 ) {
+    data class PVAData(val currentPos: () -> Double, val currentVel:() -> Double, val targetPos: Double, val targetVel: Double, val targetAcc: Double)
+
+    /**
+     * REST: resting
+     * ACC: Accelerating
+     * CONST: Const velocity
+     * DEC: Decelerating
+     */
+    enum class CurrentState { REST, ACC, CONST, DEC }
+
+    val POS_OFFSET = 100
+    val VEL_OFFSET = 100
+
+    var currentState = CurrentState.REST
+    var lastTime = 0.0
     var startTime = 0.0
 
-    var timeToZero = maxVel / maxAcc
-        get() = maxVel / maxAcc
-
     var targetPos = 0.0
-        set(value) { field = value }
+        set(value) {
+            field = value
+        }
         get() = field
 
-    var timeToMax: () -> Double = {
-        if (currentVel() == maxVel) {
-            0.0
-        } else {
-            (maxVel - currentVel()) / maxAcc
-        }
+    init {
+        lastTime = Timer.getFPGATimestamp()
     }
 
-    val hasTodecelerate = {  // assuming it's max vel, and distance it takes to zero it
-        var offset = targetPos - currentPos()
-        val time = (offset * 2) / currentVel()
-//        offset = t * currentVel() + (maxAcc/2) * Math.pow(t, 2.0)
+    val hasTodecelerate = {
+        // returns true if it has to decelerate
+        val time = currentVel() / maxAcc
+        val offset = targetPos - currentPos()
+        offset >= time * currentVel() + (maxAcc / 2) * Math.pow(time, 2.0)
     }
 
-    // d = tv + 1/2 a t^2
-    // t =
-
-    // max vel, max acc
-
-    // d = (v1 + v2) / 2 * t
-    // d / (v1 + v2) = 1 / 2 * t
 
     fun updateTarget(target: Double) {
         startTime = Timer.getFPGATimestamp()
         targetPos = target
+        currentState = CurrentState.ACC
     }
 
-    fun updateProfile(currentTime: Double = Timer.getFPGATimestamp(), currentVel: Double, currentPos: Double) {
-        val deltaTime = currentTime - startTime
+    /**
+     * @return CurrentState class depending on current velocity, and current position
+     */
+    fun updateCurrentStatus() {
+        currentState = if (hasTodecelerate()) CurrentState.DEC
+        else if (abs(currentVel()) == maxVel - VEL_OFFSET) CurrentState.CONST
+        else if (abs(currentPos()) == targetPos - POS_OFFSET) CurrentState.REST
+        else CurrentState.ACC
+    }
+
+    /**
+     * @param currentTime current time, default = Timer.getFPGATimestamp
+     * @return PVAData class which has
+     *      current position
+     *      current velocity
+     *      target position (d = Vi * t + a/2t^2)
+     *      target velocity (Vf = Vi + ta)
+     *      target acc (max theoretical acceleration, or -mac acceleration)
+     */
+    fun updateProfile(currentTime: Double = Timer.getFPGATimestamp()): PVAData  {
+        val deltaTime = currentTime - lastTime
+        lastTime = currentTime
+        updateCurrentStatus()
+        return when (currentState) {
+            CurrentState.ACC -> {
+                PVAData(
+                        currentPos,
+                        currentVel,
+                        deltaTime * currentVel() + (maxAcc/2) * Math.pow(deltaTime, 2.0),
+                        currentVel() + maxAcc * deltaTime,
+                        maxAcc
+                )
+            }
+            CurrentState.CONST -> {
+                PVAData(
+                        currentPos,
+                        currentVel,
+                        deltaTime * currentVel(),
+                        maxVel,
+                        0.0
+                )
+            }
+            CurrentState.DEC -> {
+                PVAData(
+                        currentPos,
+                        currentVel,
+                        deltaTime * currentVel() + (-maxAcc/2) * Math.pow(deltaTime, 2.0),
+                       currentVel() + -maxAcc * deltaTime,
+                        -maxAcc
+                )
+            }
+            CurrentState.REST -> {
+                PVAData(
+                        currentPos,
+                        currentVel,
+                        currentPos(),
+                        currentVel(),
+                        0.0
+                )
+            }
+        }
     }
 }
