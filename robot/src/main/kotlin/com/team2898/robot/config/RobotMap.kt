fcaf2d8f873165bcef97597116b1ff6eb70a4143
Solomon Greenberg <solomongreenberg@gmail.com>
2018-03-30 09:39:44 -0700

    Updates

--- robot/src/main/kotlin/com/team2898/robot/config/RobotMap.kt
+++ robot/src/main/kotlin/com/team2898/robot/config/RobotMap.kt
@@ -1,9 +1,9 @@
 package com.team2898.robot.config.RobotMap
 
-const val LEFT_MASTER_CANID: Int = 1
-const val LEFT_SLAVE_CANID: Int = 2
-const val RIGHT_MASTER_CANID: Int = 3
-const val RIGHT_SLAVE_CANID: Int = 4
+const val LEFT_MASTER_CANID: Int = 4
+const val LEFT_SLAVE_CANID: Int = 3
+const val RIGHT_MASTER_CANID: Int = 2
+const val RIGHT_SLAVE_CANID: Int = 1
 
 const val INTAKE_LEFT_CANID = 8
 const val INTAKE_RIGHT_CANID = 7
